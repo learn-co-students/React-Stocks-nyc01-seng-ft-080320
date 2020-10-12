df2e416a73bafb73d0cdc55a39fb8c1743007dcb
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-10-12 11:27:18 -0400

    still have to do sorting stuff

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,7 +5,35 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+state = {
+  api : [],
+  portfolioIds : []
+}
+
+componentDidMount(){
+  fetch("http://localhost:3000/stocks")
+  .then(resp => resp.json())
+  .then(data => {
+    this.setState({api: data})
+  })
+}
+
+buyStocksClick = (id) => {
+ if(!this.state.portfolioIds.find(portId => portId === id)){
+   this.setState({portfolioIds : [...this.state.portfolioIds, id ]})
+  }
+}
+
+sellStocksClick = (id) => {
+    this.setState({
+      portfolioIds: this.state.portfolioIds.filter(stock => stock !== id)
+  })
+}
+
+
+
   render() {
+    let portfolioStocks= this.state.portfolioIds.map(id => this.state.api.find(stock => stock.id === id) )
     return (
       <div>
         <SearchBar/>
@@ -13,12 +41,12 @@ class MainContainer extends Component {
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.api} buyStocksClick={this.buyStocksClick}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={portfolioStocks} sellStocksClick={this.sellStocksClick}/>
 
             </div>
           </div>
