b43c239c390e8ba5c08e5e7579e1f1686d961d2f
jhonny689 <jhonny.chamoun@gmail.com>
2020-10-13 13:44:56 -0400

    committing the solution

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" name="sort" value="Alphabetically" checked={null} onChange={e=>props.onSortChange(e.target.value)}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" name="sort" value="Price" checked={null} onChange={e => props.onSortChange(e.target.value)}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={e=>{props.onSearchChange(e.target.value)}}>
+          <option value="">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
@@ -28,6 +29,4 @@ const SearchBar = () => {
     </div>
   );
 }
-
-
 export default SearchBar;
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -3,12 +3,30 @@ import Stock from '../components/Stock'
 
 class StockContainer extends Component {
 
+  renderStocks = () => {
+    debugger;
+    let stocksToDisplay = Object.values(this.props.stocks);
+    if(this.props.sortBy === "Alphabetically")
+      stocksToDisplay.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    else if(this.props.sortBy === "Price")
+      stocksToDisplay.sort((a,b)=> a.price > b.price? 1: -1);
+
+    if(this.props.searchBy && this.props.searchBy !== "")
+      stocksToDisplay = stocksToDisplay.filter(stock => stock.type === this.props.searchBy)
+    
+    return stocksToDisplay.map(stock => {
+      return <Stock key={stock.id} stock={stock} parent="StockContainer" togglePortfolio={this.props.stockClickHandler}/>
+    });
+  }
+
   render() {
+    //console.log("rendering StockContainer with the following stocks:",this.state);
+    //debugger;
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          this.renderStocks()
         }
       </div>
     );
