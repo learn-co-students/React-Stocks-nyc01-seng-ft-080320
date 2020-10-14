11c65fb5d5bc7874c22fd9281a8c3ddf15d99ea5
islommashanlo <islom.mashanlo@gmail.com>
2020-10-13 23:07:54 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,23 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
-
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+class Stock extends React.Component {
+  
+  clickHandler = () => {
+    this.props.stockHandler(this.props.stock)
+  }
+  
+  render() {
+    return(
+      <div>
+        <div className="card">
+          <div className="card-body" onClick={this.clickHandler}>
+            <h5 className="card-title">{this.props.stock.name}</h5>
+            <p className="card-text">{this.props.stock.ticker}: {this.props.stock.price}</p>
+          </div>
+        </div>
       </div>
-    </div>
-
-
-  </div>
-);
+    )
+  }
+}
 
 export default Stock
